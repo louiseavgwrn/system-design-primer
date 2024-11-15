66595f8b8e9d983ed113e5063fd5eb41c45e4d46
GianCode888 <liberakylegian35@gmail.com>
2024-11-15 08:16:42 +0800

    Added button in main php

--- main.php
+++ main.php
@@ -1,19 +1,29 @@
-git config --global user.email "gawaranlouiseanne@gmail.com"
-
-
-<?php 
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome Dashboard</title>
 </head>
 <body>
-    
+
+    <header>
+        <h1>Welcome Our Beloved Partners</h1>
+    </header>
+
+    <main>
+        <div>
+            <button onclick="window.location.href='spguide.php'">Sustainable Practices Guide</button>
+            <button onclick="window.location.href='eventcalendar.php'">Event Calendar</button>
+        </div>
+    </main>
+
+  
+       
+
+    <footer>
+        <p>The Guardians</p>
+    </footer>
+
 </body>
 </html>
