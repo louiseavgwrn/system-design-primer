2a741bdf4bd983f9bdf84ad13a906657c637c2c3
Louise <gawaranlouiseanne@gmail.com>
2024-11-30 20:41:57 +0800

    Updated code

--- civisualizer.php
+++ civisualizer.php
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
+
 <?php
 
 $currentYear = date("Y");
-
-
-<?php 
-// Louise part
-// Take note, front-end features lang muna ito ah
-/* Basically, guide for what will be assigned here: 
-   visualizes the potential impacts of climate change on local ecosystems, 
-   helping users understand the urgency of conservation efforts */
-//If possible wag muna mag lagay design para d kau ma stress XD
-//Message sa Gc if may katanongan - Kyle
-?>
-
-
-
 
 
 <!DOCTYPE html>
